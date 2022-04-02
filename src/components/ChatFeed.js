36293d3ec5184513f30f717394b79049539e1041
tam <cherykim123@gmail.com>
2022-04-01 20:02:28 -0700

    receipt read msg

--- src/components/ChatFeed.js
+++ src/components/ChatFeed.js
@@ -6,12 +6,28 @@ const ChatFeed = (props) => {
   const { chats, activeChat, userName, messages } = props;
   const chat = chats && chats[activeChat];
 
+  //if reciever read msg
+  const renderReadReceipts = (message, isUserMessage) =>
+    chat.people.map(
+      (person, index) =>
+        person.last_read === message.id && (
+          <div
+            key={`read_${index}`}
+            className="read-receipt"
+            style={{
+              float: isUserMessage ? "right" : "left",
+              backgroundImage:
+                person.person.avatar && `url(${person.person.avatar})`,
+            }}
+          />
+        )
+    );
   const renderMessages = () => {
     const keys = Object.keys(messages);
     return keys.map((key, index) => {
       const message = messages[key];
       const lastMessageKey = index === 0 ? null : keys[index - 1];
-      const isUserMessage = userName === message.sender.userName;
+      const isUserMessage = userName === message.sender.username;
       return (
         <div key={`msg_${index}`} style={{ width: "100%" }}>
           <div className="message-block">
@@ -31,7 +47,7 @@ const ChatFeed = (props) => {
               marginLeft: isUserMessage ? "0px" : "68px",
             }}
           >
-            read-receipts
+            {renderReadReceipts(message, isUserMessage)}
           </div>
         </div>
       );
