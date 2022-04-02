15587d5de8ee9b7516114dc240900831342a34d2
tam <cherykim123@gmail.com>
2022-04-01 19:11:02 -0700

    implement handleChange and handleSubmit

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,11 +1,22 @@
 import { useState } from "react";
-const MessageForm = () => {
-  const [value, setValue] = useState("");
+import { sendMessage, isTyping } from "react-chat-engine";
 
+const MessageForm = (props) => {
+  const [value, setValue] = useState("");
+  const { chatId, creds } = props;
   const handleChange = (event) => {
     setValue(event.target.value);
+    isTyping(props, chatId);
   };
-  const handleSubmit = () => {};
+  const handleSubmit = (event) => {
+    event.preventDefault(); //not do browser refresh onece submitting the form
+
+    const text = value.trim();
+    //message comes from the react chat engine
+    //passing threads, chatId, and text
+    if (text.length > 0) sendMessage(creds, chatId, { text });
+    setValue("");
+  };
 
   return (
     <form className="message-form" onSubmit={handleSubmit}>
