d8f5ba50e6c6761a86275d4582f144f70c5041fc
tam <cherykim123@gmail.com>
2022-04-02 17:57:55 -0700

    move out ChatEngine, render RoutesApp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+// import { ChatEngine } from "react-chat-engine";
+// import { Routes, Route } from "react-router-dom";
+// import Login from "./components/Login";
+// import Register from "./components/Register";
+// import ChatApp from "./components/ChatApp";
+import RoutesApp from "./routes";
+// import { ChatEngine } from "react-chat-engine";
+// import ChatFeed from "./components/ChatFeed";
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+// const projectID = "32f34d99-af3d-4830-9718-92751ffa16a0";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <RoutesApp />
+      </BrowserRouter>
     </div>
   );
-}
-
+};
 export default App;
