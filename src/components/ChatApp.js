11a97cdf440c778806889b8f76cbec25a8fa9446
tam <cherykim123@gmail.com>
2022-04-05 18:17:28 -0700

    set env

--- src/components/ChatApp.js
+++ src/components/ChatApp.js
@@ -1,7 +1,8 @@
 import { ChatEngine } from "react-chat-engine";
 import ChatFeed from "./ChatFeed";
 
-const projectID = "32f34d99-af3d-4830-9718-92751ffa16a0";
+const projectID = process.env.REACT_APP_PROJECTID;
+
 function ChatApp() {
   return (
     <ChatEngine
