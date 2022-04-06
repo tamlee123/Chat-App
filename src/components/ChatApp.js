1761be25dae2deb54e63ef8931bcf2554489bd22
tam <cherykim123@gmail.com>
2022-04-06 01:43:45 -0700

    adding sound for new smg

--- src/components/ChatApp.js
+++ src/components/ChatApp.js
@@ -1,6 +1,8 @@
 import { ChatEngine } from "react-chat-engine";
 import ChatFeed from "./ChatFeed";
+import dotenv from "dotenv";
 
+dotenv.config({ path: ".env" });
 const projectID = process.env.REACT_APP_PROJECTID;
 
 function ChatApp() {
@@ -11,6 +13,11 @@ function ChatApp() {
       userName={localStorage.getItem("username")}
       userSecret={localStorage.getItem("password")}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+        ).play()
+      }
     />
   );
 }
